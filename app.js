498fc76a610b5fc947b16a1428c958d1f69c3e92
Monica Choe <monicchoe@gmail.com>
2017-06-12 18:31:59 -0400

    working on get use

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ app.listen(3000, function(){
 	console.log("server listening");
 });
 
+// Put middleware in the beginning to deal with different requests
+app.use(function(request, response, next){
+	response.send("USE " + request.originalUrl);
+	next();
+});
+
 app.get('/', function(request, response, next){
 	response.send("Welcome!");
-})
+	next();
+});
+
+// 	Leave next [even in the last function] for error handling 
+app.get('/get-somethingelse', function(request, response, next){
+	response.send("GET " + request.originalUrl);
+	next();
+});
+
